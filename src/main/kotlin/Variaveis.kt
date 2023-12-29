30edf2aae58b56419a8aa42d00fe34dbd1fbec84
Keilla Vitória <keillavfb@gmail.com>
2023-12-29 01:39:13 -0300

    estudo de null safety

--- src/main/kotlin/Variaveis.kt
+++ src/main/kotlin/Variaveis.kt
@@ -16,6 +16,35 @@ fun main() {
     verifyStudentAprovation("Marta", 9.5)
     verifyStudentAprovation("João", 5.0)
     verifyStudentAprovation("Carla", 6.7)
+
+    // NULL SAFETY:
+    // Para impedir as NullPointerException, as variáveis por padrão não podem receber valor null
+    var notNull: String = "Non null" // non-null
+    // notNull = null // Gera erro de compilação
+
+    // Para tipos inferidos, o compilador tabmém assume o tipo non-null
+    var inferredNonNull = "Infered non null"
+    // inferredNonNull = null // Gera erro de compilação
+
+    // Para declarar variáveis que podem assumir valor nulo, é necessário adicionar ?
+    var nullable: String? = null // Não gera erro de compilação
+
+    // Para funções que recebem valores non-null, passar um valor nullable como atributo gera um erro de compilação
+    println(stringLengthNonNull(notNull))
+//    stringLengthNonNull(nullable) // Gera erro de compilação
+
+    // Função com condição para caso a string seja nula:
+    println(stringLengthNullable(nullable))
+}
+
+fun stringLengthNonNull(string: String): Int {
+    return string.length
+}
+
+fun stringLengthNullable(string: String?): Int {
+    // Adiciona uma condição para caso a string ser nula retornar o valor 0
+    // ?: -> Elvis operator
+    return string?.length ?: 0
 }
 
 // Exemplo de aplicação de variável val:
